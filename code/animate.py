ba04bb2f997c9925fa383eae0966a7ea8261f102
Kate Storey-Fisher <kstoreyfisher@gmail.com>
2022-06-15 20:12:19 -0400

    upate animation script

--- code/animate.py
+++ code/animate.py
@@ -11,14 +11,22 @@ from mpl_toolkits.mplot3d import Axes3D
 
 from astropy.table import Table
 import astropy.cosmology
+from astropy import units as u
 
 
 def main():
-    #fn_data = '../data/gaia_photoz.fits'
-    fn_data = '/scratch/ksf293/gaia-quasars-lss/data/gaia_photoz.fits'
+    fn_data = '../data/gaia_photoz.fits'
+    #fn_data = '/scratch/ksf293/gaia-quasars-lss/data/gaia_photoz.fits'
     format_save = 'gif'
     N_sub_str = '1e3'
-    fn_save = f'../plots/animations/quasars_N{N_sub_str}.{format_save}'
+    property_colorby = 'M_absolute_g'
+    fn_save = f'../plots/animations/quasars_N{N_sub_str}_{property_colorby}.{format_save}'
+    redshift_name = 'redshift_photoz_est'
+
+    s = 0.5
+    alpha = 0.5
+    lim = 2500
+    vmin, vmax = -28, -20
 
     print("Reading data:", fn_data)
     data = Table.read(fn_data, format='fits')
@@ -28,29 +36,48 @@ def main():
     print("Loaded data with N =", len(data))
 
     # Subsample, if desired
-    N_sub = int(N_sub_str)
+    N_sub = int(float(N_sub_str))
     data = subsample(data, N_sub)
     print("Subsampled data to N =", len(data))
 
     # Convert to cartesian
     print("Converting to cartesian")
-    add_xyz(data)
+    cosmo = astropy.cosmology.Planck15
+    add_xyz(data, cosmo, redshift_name)
 
-    # add color
-    print("Adding color")
-    add_g_rp_color(data)
+    # add properties
+    print("Adding properties")
+    #add_g_rp_color(data)
+    add_M_absolute(data, cosmo, redshift_name)
 
+    # Create an init function and the animate functions.
+    print(f"s = {s}, alpha={alpha}, lim={lim}, vmin={vmin}, vmax={vmax}")
+    anim = make_anim(data, data[property_colorby], s, alpha, lim, vmin, vmax)
+
+    print("Saving animation to", fn_save)
+    s = time.time()
+    if format_save=='gif':
+        anim.save(fn_save, writer=PillowWriter(fps=32))
+    elif format_save=='mp4':
+        anim.save(fn_save, fps=30, extra_args=['-vcodec', 'libx264'])
+    e = time.time()
+    print("Save time:", e-s)
+    
+    print("Saved!")
+
+
+def make_anim(data, c, s, alpha, lim, vmin, vmax):
     # Create a figure and a 3D Axes
     fig = plt.figure(figsize=(10,10))
     ax = Axes3D(fig)
 
-    # Create an init function and the animate functions.
     def init():
-        scat = ax.scatter(data['x'], data['y'], data['z'], s=1, alpha=0.5, c=data['g_rp'],
-                        cmap='plasma', vmin=0, vmax=1)
+        #fig = plt.figure(figsize=(10,10))
+        #ax = fig.add_subplot(projection='3d')
+        scat = ax.scatter(data['x'], data['y'], data['z'], c=c, s=s, alpha=alpha, 
+                          cmap='plasma', vmin=vmin, vmax=vmax)
         ax.axis('off')
 
-        lim = 1500
         ax.set_xlim(-lim, lim)
         ax.set_ylim(-lim, lim)
         ax.set_zlim(-lim, lim)
@@ -62,25 +89,13 @@ def main():
         return fig,
 
     # Animate
-    print("Initializing animation")
     anim = animation.FuncAnimation(fig, animate, init_func=init,
-                                frames=360, interval=20, blit=True)
-
-    print("Saving animation to", fn_save)
-     s = time.time()
-    if format_save=='gif':
-        anim.save(fn_save, writer=PillowWriter(fps=32))
-    elif format_save=='mp4':
-        anim.save(fn_save, fps=30, extra_args=['-vcodec', 'libx264'])
-    e = time.time()
-    print("Save time:", e-s)
-    
-    print("Saved!")
+                                   frames=360, interval=20, blit=True)
+    return anim
 
 
-def add_xyz(data):
-    cosmo = astropy.cosmology.Planck15
-    dist_photoz = (cosmo.comoving_distance(data['redshift_photoz_est'])*cosmo.h).value # convert to Mpc/h
+def add_xyz(data, cosmo, redshift_name):
+    dist_photoz = (cosmo.comoving_distance(data[redshift_name])*cosmo.h).value # convert to Mpc/h
     data.add_column(dist_photoz, name='distance')
 
     x, y, z = radec_to_cartesian(data['distance'], data['ra'], data['dec'])
@@ -92,6 +107,15 @@ def add_xyz(data):
 def add_g_rp_color(data):
     g_rp = np.array(data['phot_g_mean_mag']) - np.array(data['phot_rp_mean_mag'])
     data.add_column(g_rp, name='g_rp')
+
+
+def add_M_absolute(data, cosmo, redshift_name):
+    dist_for_M = (10*u.pc).to(u.Mpc)
+    distance_Mpc = data['distance']/cosmo.h
+    m_apparent = np.array(data['phot_g_mean_mag'])
+    D_luminosity = (1+data[redshift_name])*distance_Mpc*u.Mpc
+    M_absolute = m_apparent - 5*np.log10(D_luminosity/dist_for_M)
+    data.add_column(M_absolute, name='M_absolute_g')
 
 
 def subsample(data, N_sub):
